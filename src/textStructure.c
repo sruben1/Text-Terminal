b305159dcb1d81273e28768cd8ccc0ff4858b33e
Heinrich Ahrend <h.ahrend@unibas.ch>
2025-05-11 14:11:33 +0200

    Fix insertions at beginning/existing splits

--- src/textStructure.c
+++ src/textStructure.c
@@ -9,6 +9,7 @@
 /*------ Data structures for internal use ------*/
 typedef struct {
   DescriptorNode *node;
+  DescriptorNode *prevNode;
   Position startPosition; // position of the block's first character
 } NodeResult;
 
@@ -78,27 +79,32 @@ Size getItemBlock( Sequence *sequence, Position position, Atomic **returnedItemB
  * Helper function for traversing the piece table to find the node containing text at a given position. 
  */
 NodeResult getNodeForPosition(Sequence *sequence, Position position){
-  NodeResult result = {NULL, -1};
+  NodeResult result = {NULL, NULL, -1};
 
   if (sequence == NULL || position < 0){
     return result; // Error 
   }
 
   DescriptorNode* curr = sequence->pieceTable.first;
+  DescriptorNode* prev = NULL;
   int i = 0;
   while(curr != NULL){
     i += curr->size;
     if (i > position){
       result.node = curr;
+      result.prevNode = prev;
       result.startPosition = i - curr->size;
       break;
     }
+    prev = curr;
     curr = curr->next_ptr;
   }
+  
   if (position == i) {
+    result.prevNode = prev;
     result.startPosition = -2; // Special case: Position at end of the sequence requested
   }
-
+  
   return result;
 }
 
@@ -106,88 +112,75 @@ ReturnCode Insert( Sequence *sequence, Position position, wchar_t *textToInsert 
   if (sequence == NULL || textToInsert == NULL){
     return -1; // Error
   }
-
-  // If the sequence is empty, handle first insertion separately
-  if (sequence->pieceTable.length == 0 && position == 0){
-    if (writeToAddBuffer(sequence, textToInsert) == -1){
-      return -1; // Error: Failed to write to buffer
-    }
-    DescriptorNode* firstNode = (DescriptorNode*) malloc(sizeof(DescriptorNode));
-    firstNode->isInFileBuffer = false;
-    firstNode->offset = 0;
-    firstNode->size = (long int) getUtf8ByteSize(textToInsert);
-    firstNode->next_ptr = NULL;
-
-    sequence->pieceTable.first = firstNode;
-    sequence->pieceTable.length = 1;
-
-    return 1;
-  }
-
-  NodeResult nodeResult = getNodeForPosition(sequence, position);
-  DescriptorNode* firstNode = nodeResult.node;
-  if (firstNode == NULL){
-    return -1; // Error: Position out of bounds
-  }
-
-  int distanceInBlock = position - nodeResult.startPosition;
+  
+  // Write the text to the add buffer and get the offset
   int newlyWrittenBufferOffset = writeToAddBuffer(sequence, textToInsert);
   if (newlyWrittenBufferOffset == -1){
     ERR_PRINT("Insert failed at write to add buffer.\n");
-    return -1; // Error: Failed to write to buffer
+    return -1;
   }
 
-  DescriptorNode* previous = firstNode;
-  DescriptorNode* current = firstNode;
-  int summedSizes = 0;
-  while(current != NULL){
-    summedSizes += current->size;
-    // Reached node of or exactly at "position": 
-    if(position < summedSizes){ 
-      // Undo last operation:
-      summedSizes -= current->size;
-      break;
-    }
-    previous = current;
-    current = current->next_ptr; 
-  }
-
+  // Create a new node for the inserted text
   DescriptorNode* newInsert = (DescriptorNode*) malloc(sizeof(DescriptorNode));
   if(newInsert == NULL){
     ERR_PRINT("Fatal malloc fail at insert operation!\n");
     return -1;
   }
-
-  // Updated newly created node with the new insert:
   newInsert->isInFileBuffer = false;
   newInsert->offset = newlyWrittenBufferOffset;
   newInsert->size = (long int) getUtf8ByteSize(textToInsert);
-  
-  // >> Piece Table update cases <<
-  if(position == summedSizes -1){  //Case of insert at already existing piece table split:
-    DEBG_PRINT("Insert at already existing piece table split.\n");
-    // Update piece table state:
-    previous->next_ptr = newInsert;
-    //(if end of table: current == NULL, so this behavior ok):
-    newInsert->next_ptr = current;
 
+  // Seperately handle insertion at the beginning of the sequence
+  if (position == 0){
+    DEBG_PRINT("Insert at beginning of sequence.\n");
+    newInsert->next_ptr = sequence->pieceTable.first; // Potentially NULL for empty sequence
+    sequence->pieceTable.first = newInsert;
     sequence->pieceTable.length += 1;
-  } else {//Case of insert position within an existing piece:
-    // Need to split into 3 pieces:
-    // convert current as first part and put remaining in seccond:
+    return 1;
+  }
+
+  // Otherwise find the node for the given position
+  NodeResult nodeResult = getNodeForPosition(sequence, position);
+
+  // Insert the new node into the piece table
+  if (nodeResult.startPosition == -2 || nodeResult.startPosition == position){
+    // Position is at already existing piece table split
+    DEBG_PRINT("Insert at already existing piece table split.\n");
+
+    DescriptorNode* prev = nodeResult.prevNode;
+    if (prev == NULL) {
+      ERR_PRINT("Fatal error: prev node is NULL!\n");
+      free(newInsert);
+      return -1; // Error: Invalid state
+    }
+    prev->next_ptr = newInsert;
+    newInsert->next_ptr = nodeResult.node; // also works at end of table => nodeResult.node == NULL
+    sequence->pieceTable.length += 1;
+
+  } else {
+    // Position is within an existing piece
+    DEBG_PRINT("Insert within an existing piece.\n");
+
+    // Split the existing node into two parts
+    DescriptorNode* firstPart = nodeResult.node;
     DescriptorNode* seccondPart = (DescriptorNode*) malloc(sizeof(DescriptorNode)); 
-    seccondPart->size = current->size - (position - summedSizes); //set size of inverse of first part.
-    current->size = position - summedSizes; //set size of first part.
-    // current already at right offset since essentially truncating it.
-    seccondPart->offset =  current->offset + current->size;
+    if (firstPart == NULL || seccondPart == NULL){
+      ERR_PRINT("Fatal malloc fail at insert operation!\n");
+      return -1;
+    }
+    int distanceInBlock = position - nodeResult.startPosition;
+    seccondPart->isInFileBuffer = firstPart->isInFileBuffer;
+    seccondPart->size = firstPart->size - distanceInBlock; // set size as inverse of first part.
+    seccondPart->offset = firstPart->offset + distanceInBlock;
+    firstPart->size = distanceInBlock;
     
     // Update the piece table
-    seccondPart->next_ptr = current->next_ptr;
+    seccondPart->next_ptr = firstPart->next_ptr;
     newInsert->next_ptr = seccondPart;
-    current->next_ptr = newInsert;
-
+    firstPart->next_ptr = newInsert;
     sequence->pieceTable.length += 2;
   }
+
   return 1;
 }
 
